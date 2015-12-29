bd2653955766e5e661f444afa8865f1a17c8e6d7
Cigano Morrison Mendez <ciganogypsy@gmail.com>
2015-12-29 17:58:51 -0200

    Correção e aplicação da prova de conceito.

--- TesteDropDownEnum/TesteDropDownEnum/Controllers/HomeController.cs
+++ TesteDropDownEnum/TesteDropDownEnum/Controllers/HomeController.cs
@@ -14,13 +14,14 @@ namespace TesteDropDownEnum.Controllers
             {
                 Item = MeuEnum.Item6
             };
-            p.Teste = new List<Teste>();
 
-            p.Teste.Add(new Teste() { item = MeuEnum.Item0 });
-            p.Teste.Add(new Teste() { item = MeuEnum.Item1 });
-            p.Teste.Add(new Teste() { item = MeuEnum.Item2 });
-            p.Teste.Add(new Teste() { item = MeuEnum.Item3 });
-
+            p.Testes = new List<Teste> { 
+                new Teste() { item = MeuEnum.Item0 },
+                new Teste() { item = MeuEnum.Item1 },
+                new Teste() { item = MeuEnum.Item2 },
+                new Teste() { item = MeuEnum.Item3 }
+            };
+            
             return View(p);
         }
     }
@@ -28,7 +29,7 @@ namespace TesteDropDownEnum.Controllers
     public class Principal
     {
         public MeuEnum Item { get; set; }
-        public List<Teste> Teste { get; set; }
+        public List<Teste> Testes { get; set; }
     }
 
     public class Teste
@@ -39,21 +40,13 @@ namespace TesteDropDownEnum.Controllers
     public enum MeuEnum
     {
         Item0 = 0,
-
         Item1 = 1,
-
         Item2 = 2,
-
         Item3 = 3,
-
         Item4 = 4,
-
         Item5 = 5,
-
         Item6 = 6,
-
         Item7 = 7,
-
         Item8 = 8,
     }
 
@@ -71,19 +64,16 @@ namespace TesteDropDownEnum.Controllers
             return new SelectList(lista);
         }
 
-        public static SelectList MeuEnumItens(this HtmlHelper html, MeuEnum? item = null)
+        public static IEnumerable<SelectListItem> MeuEnumItens(this HtmlHelper html, MeuEnum? itemSelecionado)
         {
-            var item0 = new SelectListItem() { Value = ((int)MeuEnum.Item0).ToString(), Selected = item != null ? item == MeuEnum.Item0 : false, Text = "0" };
-            var item1 = new SelectListItem() { Value = ((int)MeuEnum.Item1).ToString(), Selected = item != null ? item == MeuEnum.Item1 : false, Text = "1" };
-            var item2 = new SelectListItem() { Value = ((int)MeuEnum.Item2).ToString(), Selected = item != null ? item == MeuEnum.Item2 : false, Text = "2" };
-            var item3 = new SelectListItem() { Value = ((int)MeuEnum.Item3).ToString(), Selected = item != null ? item == MeuEnum.Item3 : false, Text = "3" };
-            var item4 = new SelectListItem() { Value = ((int)MeuEnum.Item4).ToString(), Selected = item != null ? item == MeuEnum.Item4 : false, Text = "4" };
-            var item5 = new SelectListItem() { Value = ((int)MeuEnum.Item5).ToString(), Selected = item != null ? item == MeuEnum.Item5 : false, Text = "5" };
-            var item6 = new SelectListItem() { Value = ((int)MeuEnum.Item6).ToString(), Selected = item != null ? item == MeuEnum.Item6 : false, Text = "6" };
-            var item7 = new SelectListItem() { Value = ((int)MeuEnum.Item7).ToString(), Selected = item != null ? item == MeuEnum.Item7 : false, Text = "7" };
-            var item8 = new SelectListItem() { Value = ((int)MeuEnum.Item8).ToString(), Selected = item != null ? item == MeuEnum.Item8 : false, Text = "8" };
+            var lista = new List<SelectListItem>();
 
-            return new SelectList(new[] { item0, item1, item2, item3, item4, item5, item6, item7, item8 }, "Value", "Text");
+            foreach (MeuEnum item in System.Enum.GetValues(typeof(MeuEnum)))
+            {
+                lista.Add(new SelectListItem() { Text = item.ToString("F"), Value = item.ToString(), Selected = (itemSelecionado.HasValue ? itemSelecionado.Value == item : false) });
+            }
+
+            return lista;
         }
 
         public static IEnumerable<SelectListItem> MeuEnumItens2(this HtmlHelper html, MeuEnum? item = null)
